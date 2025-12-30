24011d362c990af0ee01ce0f5fa50cb702271f9c
Sathish Vadivel <117635296+Sathish-Vadivel@users.noreply.github.com>
2025-12-30 21:25:35 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -4,20 +4,20 @@ from calculator import add, subtract, multiply, divide
 class TestCalculator(unittest.TestCase):
     
     def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(2, 3), 5)      # Passes
+        self.assertEqual(add(2, 3), 6)      # Fails! (This is the bug)
     
     def test_subtract(self):
-        self.assertEqual(subtract(5, 3), 2)
-        self.assertEqual(subtract(0, 4), -4)
+        self.assertEqual(subtract(5, 3), 2) # Passes
+        self.assertEqual(subtract(5, 5), 3) # Fails! (This is the bug)
     
     def test_multiply(self):
-        self.assertEqual(multiply(3, 4), 12)
-        self.assertEqual(multiply(-2, 3), -6)
+        self.assertEqual(multiply(3, 4), 12)    # Passes
+        self.assertEqual(multiply(3, 0), 5)      # Fails! (This is the bug)
     
     def test_divide(self):
-        self.assertEqual(divide(10, 2), 5)
-        self.assertRaises(ValueError, divide, 10, 0)
+        self.assertEqual(divide(10, 2), 5)      # Passes
+        self.assertRaises(ValueError, divide, 10, 0)   # Passes (dividing by zero)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
